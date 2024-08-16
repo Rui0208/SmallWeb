fc2d0abe8cea8b78f75461536e588c4da5c5e3e7
Nioh <niohwang@aetheras.io>
2024-08-16 13:04:03 +0800

    end

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -25,7 +25,7 @@ const config: Config = {
     },
   },
   plugins: [
-    function ({ addUtilities }) {
+    function ({ addUtilities }: any) {
       const newUtilities = {
         '.vertical-rl': {
           writingMode: 'vertical-rl',
