06ac49d4520ac9aa2a6722232f6b29f765539e20
Nioh <niohwang@aetheras.io>
2025-01-22 15:36:00 +0800

    update

--- src/pages/bingo-game.tsx
+++ src/pages/bingo-game.tsx
@@ -387,8 +387,8 @@ export default function BingoGame() {
                             cell.marked
                               ? 'bg-blue-500 text-white'
                               : cell.value
-                                ? 'bg-white text-blue-900 hover:bg-blue-100'
-                                : 'bg-white/10 text-white hover:bg-white/20'
+                                ? 'bg-blue-100 text-blue-900 hover:bg-blue-200'
+                                : 'bg-blue-50/10 text-white hover:bg-blue-50/20'
                           }`}
                       >
                         {cell.value}
@@ -438,8 +438,8 @@ export default function BingoGame() {
                             cell.marked
                               ? 'bg-blue-500 text-white'
                               : cell.value
-                                ? 'bg-white text-blue-900 hover:bg-blue-100'
-                                : 'bg-white/10 text-white hover:bg-white/20'
+                                ? 'bg-blue-100 text-blue-900 hover:bg-blue-200'
+                                : 'bg-blue-50/10 text-white hover:bg-blue-50/20'
                           }`}
                       >
                         {cell.value}
